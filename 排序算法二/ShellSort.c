245784cca99bd0199b2828ff22349343198d7803
浅忆梦微凉 <3538922590@qq.com>
2018-10-11 21:20:03 +0800

    ShellSort

--- 排序算法二/ShellSort.c
+++ 排序算法二/ShellSort.c
@@ -2,7 +2,30 @@
 #include<Windows.h>
 #include<time.h>
 
-void ShellSort(int array[], int gap,int size)
+void swap(int *p, int *q)
+{
+	int *tmp = *p;
+	*p = *q;
+	*q = tmp;
+}
+
+void ShellSort2(int array[], int gap, int size)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	for (k = gap; k > 0; k--)
+	{
+		for (i = 0; i < size; i++)
+		{
+			j = i + k;
+			if ((j<size) && (array[i]>array[j]))
+				swap(&array[i], &array[j]);
+		}
+	}
+}
+
+void ShellSort1(int array[], int gap,int size)
 {
 	int i = 0;
 	int j = 0;
@@ -28,7 +51,7 @@ void test()
 	int array[10] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 	int size = sizeof(array) / sizeof(array[0]);
 	int gap = size / 3 + 1;
-	ShellSort(array, gap,size);
+	ShellSort2(array, gap,size);
 	for (int i = 0; i < size; i++)
 	{
 		printf("%d ", array[i]);
